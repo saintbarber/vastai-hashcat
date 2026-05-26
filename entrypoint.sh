04727352ca55c50f90dbb20f40a0cabcae1f7802
saintbarber <saintbarb3r@gmail.com>
2026-05-26 17:22:46 +0300

    removed wordlist - migrate to vast-ai volumes

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,21 @@ service ssh start
 DIRECTORY="wordlists"
 
 # Loop through all .7z files in the directory
-for file in "$DIRECTORY"/*.7z; do
-  # Check if there are any .7z files
-  [ -e "$file" ] || continue
 
-  echo "Extracting: $file" 
+# TODO - Find solution for wordlist hosting
+
+
+# for file in "$DIRECTORY"/*.7z; do
+#   # Check if there are any .7z files
+#   [ -e "$file" ] || continue
+
+#   echo "Extracting: $file" 
   
-  # Extract the .7z file to a folder with the same name (without extension)
-  7zz x "$file" -o"$DIRECTORY"
+#   # Extract the .7z file to a folder with the same name (without extension)
+#   7zz x "$file" -o"$DIRECTORY"
 
-  echo "Done: $file"
-  rm "$file" 
-done
+#   echo "Done: $file"
+#   rm "$file" 
+# done
 
 sleep infinity # Needed to keep container open
